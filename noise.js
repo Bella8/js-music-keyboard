d0d6be4b3fe894fc3402cfa7e3931ccaa4e3840d
Beylul <bkbreab8@gmail.com>
2016-11-22 07:25:58 -0800

    keyboard working on click and keypress

--- noise.js
+++ noise.js
@@ -1,3 +1,17 @@
+var note = function(select){
+  var audio = $("#" + select + "Audio")[0];
+  audio.load();
+  audio.play();
+};
+
 $(document).ready( function() {
-  // your code here
+  $('.note').on('click', function() {
+    var select = $(this).html();
+    note(select);
+  });
+
+  $('.note').on('keydown', function(event) {
+    var select = event.key;
+    note(select);
+  });
 });
